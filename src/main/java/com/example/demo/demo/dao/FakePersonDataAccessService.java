0821b3a213d87ab06ccf66c177499987a8e0b7cf
Famil <frostgamingrespawn@gmail.com>
2021-02-06 15:15:29 +0100

    added dependency injections

--- src/main/java/com/example/demo/demo/dao/FakePersonDataAccessService.java
+++ src/main/java/com/example/demo/demo/dao/FakePersonDataAccessService.java
@@ -1,10 +1,12 @@
 package com.example.demo.demo.dao;
 import com.example.demo.demo.model.Person;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+@Repository("fakeDao")
 public class FakePersonDataAccessService implements PersonDao {
 
     private static List<Person> DB = new ArrayList<>();
